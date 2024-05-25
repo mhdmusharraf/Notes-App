b1124ff1a5e034cf2dbebb075cba510a9ba50111
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-05-25 23:51:54 +0530

    Created Account API

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,14 @@ const mongoose = require("mongoose");
 
 mongoose.connect(config.connectionString);
 
+const User = require("./models/user.model");
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
 const jwt = require("jsonwebtoken");
-const {authenticateToken} = require("./utilities");
+const { authenticateToken } = require("./utilities");
 
 app.use(express.json());
 
@@ -25,6 +27,48 @@ app.get("/", (req, res) => {
 });
 
 // Create Account
+app.post("/create-account", async (req, res) => {
+  const { fullName, email, password } = req.body;
+  if (!fullName) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Full Name is required" });
+  }
+
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Email is required" });
+  }
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is required" });
+  }
+
+  const isUser = await User.findOne({ email: email });
+  if (isUser) {
+    return res.json({ error: true, message: "User already exists" });
+  }
+
+  const newUser = new User({
+    fullName,
+    email,
+    password,
+  });
+
+  await newUser.save();
+
+  const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "36000m",
+  });
+
+  return res.json({
+    error: false,
+    user,
+    accessToken,
+    message: "Account created successfully",
+  });
+});
 
 app.listen(8000);
 
